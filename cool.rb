39992cc7e937f99cd1b563846d4b5a9b1d96e48d
Jay <jpnappy@gmail.com>
2016-10-23 15:09:56 -0400

    adding requirements

--- cool.rb
+++ cool.rb
@@ -2,8 +2,20 @@
 #if the location is equal to brooklyn, show the user a message "you are so cool"
 #if the location is equal to manhattan, show the user a message "you are cool"
 #in all other circumstances, show a message that says "you are not cool" 
+def location_checker(location)
+	if location == "brooklyn" or location == "manhattan"
+		puts("you are so cool")
+	elsif location == "queens" or location == "bronx" 
+		puts("you are cool")
+	else 
+		puts("you are not cool")
+	end
+end
 
 # ask the user for where they live
+puts("where do you live?")
 # store that information in a variable
+user_location = gets.strip
 
 # call the fuction
+location_checker(user_location)
